440081edf20550360aed28d8f455750dcd318bea
Chandan93411 <125748747+Chandan93411@users.noreply.github.com>
2023-05-29 21:23:15 +0530

    Add files via upload

---
+++ CommandLineDemo.java
@@ -0,0 +1,12 @@
+
+public class CommandLineDemo
+{ 
+    public static void main(Strin args[])
+
+    {
+            for (int i = 0; i < args.length; i++)
+                System.out.println("args[+ i +]: ", + args[i]);
+
+    }
+
+}
