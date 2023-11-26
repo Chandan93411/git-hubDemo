12e6ce0dc838447d83d4850aaba89b5a4f928ac4
Chandan93411 <Chandan93411@gmail.com>
2023-11-27 01:50:51 +0530

    Architect Asian Development

--- CommandLineDemo.java
+++ CommandLineDemo.java
@@ -6,6 +6,7 @@ public class CommandLineDemo
     {
             for (int i = 0; i < args.length; i++)
                 System.out.println("args[+ i +]: ", + args[i]);
+            System.out.println("Test java");
 
     }
 
